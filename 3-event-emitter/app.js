2ee3d808ba00425124a4646addf972e03452774b
Mark Ormesher <me@markormesher.co.uk>
2016-01-21 21:27:11 +0000

    Constant event names

--- 3-event-emitter/app.js
+++ 3-event-emitter/app.js
@@ -1,18 +1,22 @@
+// event names from config
+
+var config = require('./config');
+
 // DIY version
 
 var DiyEmitter = require('./emitter');
 
 var diyEmtr = new DiyEmitter();
 
-diyEmtr.on('event1', function() {
+diyEmtr.on(config.events.EVENT_1, function() {
 	console.log('event1 happened! (reported by listener 0)');
 });
 
-diyEmtr.on('event1', function() {
+diyEmtr.on(config.events.EVENT_1, function() {
 	console.log('event1 happened! (reported by listener 1)');
 });
 
-diyEmtr.emit('event1');
+diyEmtr.emit(config.events.EVENT_1);
 
 // Node.js version
 
@@ -20,12 +24,12 @@ var NodeEmitter = require('events');
 
 var nodeEmtr = new NodeEmitter();
 
-nodeEmtr.on('event2', function() {
+nodeEmtr.on(config.events.EVENT_2, function() {
 	console.log('event2 happened! (reported by listener 0)');
 });
 
-nodeEmtr.on('event2', function() {
+nodeEmtr.on(config.events.EVENT_2, function() {
 	console.log('event2 happened! (reported by listener 1)');
 });
 
-nodeEmtr.emit('event2');
+nodeEmtr.emit(config.events.EVENT_2);
